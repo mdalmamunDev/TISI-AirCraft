573a9f4a290738e72ea6eff459ec8c195e6c7889
Md. Al Mamun <118971184+mdalmamun5354@users.noreply.github.com>
2022-12-20 20:35:10 +0600

    Add files via upload

---
+++ java/com/example/tisiaircraftByMamun/Bullet.java
@@ -0,0 +1,39 @@
+package com.example.tisiaircraftByMamun;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+import static com.example.tisiaircraftByMamun.GameView.screenRatioX;
+import static com.example.tisiaircraftByMamun.GameView.screenRatioY;
+//import static com.heyletscode.ihavetofly.GameView.screenRatioX;
+//import static com.heyletscode.ihavetofly.GameView.screenRatioY;
+
+public class Bullet {
+
+    int x, y, width, height;
+    Bitmap bullet;
+
+    Bullet (Resources res) {
+
+        bullet = BitmapFactory.decodeResource(res, R.drawable.bullet);
+
+        width = bullet.getWidth();
+        height = bullet.getHeight();
+
+        width /= 4;
+        height /= 4;
+
+        width = (int) (width * screenRatioX);
+        height = (int) (height * screenRatioY);
+
+        bullet = Bitmap.createScaledBitmap(bullet, width, height, false);
+
+    }
+
+    Rect getCollisionShape () {
+        return new Rect(x, y, x + width, y + height);
+    }
+
+}
